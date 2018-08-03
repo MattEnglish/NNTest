d97d2045d5264c9b68a028663a84fd6d0cd3448b
Matt English <matthew.english@softwire.com>
2018-08-03 01:01:35 +0100

    first commit

---
+++ NeuralNetTest/AccordTest.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accord.Neuro;
+using Accord.Math;
+using Accord.Statistics;
+using Accord.Neuro.Learning;
+using Accord.Genetic;
+using Accord.Math.Random;
+
+namespace NeuralNetTest
+{
+
+    public class learnData
+    {
+        public double currentDrawStreak;
+        public double MyWinsLeft;
+        public double EnemyWinsLeft;
+        public double MyDynamiteLeft;
+        public double EnemyDynamiteLeft;
+    }
+
+    public class fitFunc: IFitnessFunction
+    {
+        public double Evaluate(IChromosome chromosone)
+        {
+            var test = (DoubleArrayChromosome)chromosone;
+            var value = test.Value;
+            var net = new NeuralNetworks();
+
+            for (int i = 0; i < 6; i++)
+            {
+                 net.W1[i/3,i%3] = value[i];
+            }
+            for (int i = 6; i < 9; i++)
+            {
+                net.W2[i -6, 0] = value[i];
+            }
+
+            var a = Math.Pow(net.Forward(new double[1, 2] { { 0, 1 } })[0,0] -1,2);
+            var b = Math.Pow(net.Forward(new double[1, 2] { { 1, 0 } })[0, 0] - 1, 2);
+            var c = Math.Pow(net.Forward(new double[1, 2] { { 0, 0 } })[0, 0], 2);
+            var d = Math.Pow(net.Forward(new double[1, 2] { { 1, 1 } })[0, 0], 2);
+
+            return Math.Max( 1 - a - b - c - d, 0);
+
+        }
+    }
+
+
+    public class AccordTest
+    {
+        public static void RunGene()
+        {
+            var rand = new ZigguratExponentialGenerator();
+            var chrom = new DoubleArrayChromosome(rand, rand, rand, 9 );
+            var pop = new Population(1000, chrom, new fitFunc(), new EliteSelection() );
+            Console.WriteLine(pop.FitnessMax);
+            for (int i = 0; i < 199; i++)
+            {
+                pop.RunEpoch();
+                Console.WriteLine(pop.FitnessMax);
+            }
+            var x = new fitFunc().Evaluate(pop.BestChromosome);
+
+            Console.WriteLine(pop.FitnessMax);
+        }
+
+
+        public static void Run()
+        {
+            double[][] input =
+            {
+                new double[] {0, 0}, new double[] {0, 1},
+                new double[] {1, 0}, new double[] {1, 1}
+            };
+
+            double[][] output =
+            {
+                new double[] {0}, new double[] {1},
+                new double[] {1}, new double[] {0}
+            };
+            var network = new ActivationNetwork(new SigmoidFunction(2), 2, 4, 1);
+            var y = network.Compute(new double[] { 1, 0 });
+            var teacher = new LevenbergMarquardtLearning(network);
+
+            
+            teacher.RunEpoch(input, output);
+            var z = network.Compute(new double[] { 1, 0 });
+
+            teacher.RunEpoch(input, output);
+            teacher.RunEpoch(input, output);
+            teacher.RunEpoch(input, output);
+            teacher.RunEpoch(input, output);
+            var badsf = network.Compute(new double[] { 1, 0 });
+            var test2 = network.Compute(new double[] { 1, 1 });
+        }
+
+    }
+}
---
+++ NeuralNetTest/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accord.Neuro;
+using Accord.Math;
+
+namespace NeuralNetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AccordTest.RunGene();
+            var x = new NeuralNetworks();
+            var data = new double [3, 2] { { 1.3, 0.2} , { 0.5,1.1}, { 0.4,4.4} };
+            var y = x.Forward(data);
+
+
+        }
+    }
+
+    public class NeuralNetworks
+    {
+        private int inputLayerSize = 2;
+        private int outputLayerSize = 1;
+        private int hiddenLayerSize = 3;
+
+        public double[,] W1;
+        public double[,] W2;
+
+        private double[,] dJdW1;
+        private double[,] dJdW2;
+
+        private static double costFunction( double[,] y, double[,] ry)
+        {
+            var subtracted = Elementwise.Subtract(y, ry);
+            var differnce = ApplyFuncToEveryElement(subtracted, x => 0.5* Math.Pow(x,2));
+            return Matrix.Sum(differnce);
+        }
+
+        public static double sigmoid(double z)
+        {
+            return 1 / (1 + Math.Exp(-z));
+        }
+
+        public static double SigmoidPrime(double z)
+        {
+            return Math.Exp(-z) / Math.Pow((1 + Math.Exp(-z)), 2);
+        }
+
+        public static double[,] sigmoid(double[,] z)
+        {
+            for (int i = 0; i < z.GetLength(0); i++)
+            {
+                for (int j = 0; j < z.GetLength(1); j++)
+                {
+                    z[i, j] = sigmoid(z[i, j]);
+                }
+            }
+            return z;
+        }
+
+        private static double[,] ApplyFuncToEveryElement(double[,] z, Func<double, double> func )
+        {
+            for (int i = 0; i < z.GetLength(0); i++)
+            {
+                for (int j = 0; j < z.GetLength(1); j++)
+                {
+                    z[i, j] = func(z[i, j]);
+                }
+            }
+            return z;
+        }
+
+        
+        public NeuralNetworks()
+        {
+            W1 = new double[inputLayerSize, hiddenLayerSize];
+            W2 = new double[hiddenLayerSize, outputLayerSize];
+            var r = new Random();
+
+            ApplyFuncToEveryElement(W1, x => r.NextDouble());
+            ApplyFuncToEveryElement(W2, x => r.NextDouble());
+
+        }
+
+        public double[,] Forward(double[,] inputs)
+        {
+            var x = Matrix.Dot(inputs, W1);
+            var a = sigmoid(x);
+            var y = Matrix.Dot(x, W2);
+            var b = sigmoid(y);
+            return b;
+        }
+
+
+        private void input(int[] x)
+        {
+
+        }
+    }
+}
